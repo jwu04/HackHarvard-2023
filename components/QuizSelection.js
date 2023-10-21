888ac0981290d3bc70995488fef0aff9f43d1164
Jonathan Wu <wu.jonathan09@gmail.com>
2023-10-21 19:52:42 -0400

    add stability ai api integration

--- components/QuizSelection.js
+++ components/QuizSelection.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import Emoji from "./Emoji";
 
 const QuizSelection = ({ emoji, caption, onClick }) => {
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import QuizState from "../components/states/QuizState.js"
 import SB from "../components/SB.js"
 import DashboardState from "../components/states/DashboardState";
+import GenerateImage from "../components/Generation.js";
 
 export default function Home() {
     return (
         <>
             <DashboardState/>
+            <GenerateImage/>
             {/*<SB/>*/}
         </>
     )
