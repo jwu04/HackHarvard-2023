6edd7da26b3d6720aa7afdc1591855bda3c56809
Amy Mai <am2556@cornell.edu>
2023-10-21 16:04:40 -0400

    using env variables instead

--- pages/api/getSB.js
+++ pages/api/getSB.js
@@ -1,16 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://exkudtzambimhqmbrrev.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4a3VkdHphbWJpbWhxbWJycmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5MDc3MTQsImV4cCI6MjAxMzQ4MzcxNH0.s3yzcYjZlMUhS0GdUutayWDMGvQsUXPZnetcAKLH3co'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const supabaseAuth = async (req, res) => {
 
     const { data, error } = await supabase
         .from('memories')
-        .insert([
-            { name: 'hehea' },
-        ])
         .select()
 
     if (error) {
