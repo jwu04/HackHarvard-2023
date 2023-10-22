e20f91db9095152686d693b6f7d82ebf5340cb33
a <a@a.a>
2023-10-22 00:37:26 -0400

    Feature overhaul

--- components/states/DashboardState.js
+++ components/states/DashboardState.js
@@ -5,24 +5,28 @@ import Link from "next/link";
 
 const DashboardState = () => {
     const memories = [
-        { description: "Acidic", img: "memory1.jpg" },
-        { description: "Shrooms", img: "memory2.jpg" },
-        { description: "Bathed", img: "memory3.jpg" },
-        { description: "In", img: "memory4.jpg" },
-        { description: "Chocolate", img: "memory5.jpg" },
-        { description: "Milk", img: "memory6.jpg" }
+        { date: "Oct 21", description: "Acidic", img: "memory1.jpg" },
+        { date: "Oct 21", description: "Shrooms", img: "memory2.jpg" },
+        { date: "Oct 21", description: "Bathed", img: "memory3.jpg" },
+        { date: "Oct 21", description: "In", img: "memory4.jpg" },
+        { date: "Oct 21", description: "Chocolate", img: "memory5.jpg" },
+        { date: "Oct 21", description: "Milk", img: "memory6.jpg" }
     ];
 
     return (
         <>
             <div className="sec-body">
+                <p className="text-center font-bold"><span className="logo">rememory</span><p className="relative left-80 bottom-20 text-lg red-logo">(remember me)</p></p>
                 <div className="flex flex-wrap gap-8">
                     {memories.map((memory, index) => (
-                        <div key={index} className="border border-red-400 rounded-md font-bold text-lg p-2 px-6 mb-3">
-                            Memory #{index + 1} - {memory.description}
+                        <div key={index} className="border-2 shadow-2xl border-gray-800 rounded-md py-2 px-8">
+                            <p className="font-black text-2xl">{memory.date}</p> <p className="text-xl">{memory.description}</p>
                         </div>
                     ))}
                 </div>
+
+                Rememory.
+                Remember me.
 
             </div>
         </>
--- pages/api/getQuestions.js
+++ pages/api/getQuestions.js
@@ -1,27 +1,40 @@
-import OpenAI from "openai";
-const openai = new OpenAI();
+import { create } from 'openai'
 
 const getQs = async (req, res) => {
     const answers = req.body.answers
-    console.log(answers[0])
 
-    let prompt = 'We are writing a story based on someone\'s memory. ' +
-    `So far, we know that it is about ${(answers[0]).a.toLowerCase()} and it is a ${(answers[1]).q.toLowerCase()} memory.` +
-    'We also have the answers to the following questions:'
+    let prompt = `We are writing a story based on someone's memory. ` +
+        'So far, we know that it is about ' + answers[0].a + ' and it is a ' + answers[1].a + ' memory.' +
+        'We also have the answers to the following questions:';
 
     for (let i = 2; i < answers.length; i++) {
-        prompt += `\nQ: ${answers[i].Q} \nA: ${answers[i].A}`
+        prompt += `\nQ: ${answers[i].q} \nA: ${answers[i].a}`
     }
 
-    prompt += "\n\nWhat is one potential question you could ask this person to hear more about their memory? Start it off by saying, in a counselor's voice, \"So I've gathered that you <context> and <context>-\" then delve deeper into one final question to complete your psychoanalysis."
-    const nextQ = await openai.completions.create({
-        model: 'gpt-3.5-turbo-instruct',
-        prompt: prompt
-    }).then(() => {
-        res.status(200).json({ message: nextQ })
-    }).catch(err => {
-        res.status(404).json({ success: 'false' })
+    prompt += "\n\nWhat is one potential question you could ask this person to hear more about their memory? Start it off by saying, in a counselor's voice, 'So I've gathered that you (context) and (context)-' then delve deeper into one final question to complete your psychoanalysis. However, please be specific, especially pertaining to the last answer that they gave. Make them feel that you care. Be concise."
+    const apiKey = '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'
+    const url = 'https://api.openai.com/v1/chat/completions'
+
+    const body = JSON.stringify({
+        messages: [{"role": "user", "content": prompt}],
+        model: 'gpt-3.5-turbo',
+        stream: false,
     })
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${apiKey}`,
+            },
+            body,
+        })
+        const data = await response.json()
+        res.status(200).json({message: data})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 }
 
 export default getQs;
