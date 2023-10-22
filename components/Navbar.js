3268cf5bd2a478a7396962959074536d846e4a1a
a <a@a.a>
2023-10-22 02:05:17 -0400

    Appearance overhaul

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,14 +11,25 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="sec-body">
-            <div className="mb-3">
-                <div className="flex flex-row ml-auto display-none items-center lg:w-auto text-center w-32">
-                    <div
-                        className={"select-none nav-font colored coloredChosen text-lg text-white h-full theme-bg rounded-md px-16 py-2"}
-                        draggable="false" onClick={handleRedirect("/quiz")}>
-                        <span className={'font-semibold'}>Write Memory</span>
-                    </div>
+        // TODO: remove colored and coloredChosen
+        <nav className="py-5 shadow-sm mb-10">
+            <div className="sec-body mt-3 mb-3">
+                <div className="flex flex-row ml-auto display-none items-center lg:w-auto text-center">
+                    <p className="text-center font-bold"><span className="logo">Rememory</span></p>
+                    {currentPage === '/' ?
+                        <div
+                            className={"ml-auto cursor-pointer nav-font colored coloredChosen text-lg text-white h-full theme-bg rounded-lg hover:opacity-80 transition duration-100 ease-in-out transform px-12 py-2"}
+                            draggable="false" onClick={handleRedirect("/quiz")}>
+                            <span className={'font-semibold'}><i
+                                className="fa-solid fa-plus"></i>&nbsp; Write Memory</span>
+                        </div>
+                        :
+                        <div
+                            className={"ml-auto cursor-pointer nav-font colored coloredChosen text-lg text-white h-full theme-bg rounded-lg hover:opacity-80 transition duration-100 ease-in-out transform px-12 py-2"}
+                            draggable="false" onClick={handleRedirect("/")}>
+                            <span className={'font-semibold'}><i className="fa-solid fa-list"></i>&nbsp; View Memories</span>
+                        </div>
+                    }
                 </div>
             </div>
         </nav>
