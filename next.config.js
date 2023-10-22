c84979e6cc0111940c2ddee7d22036d5446edf78
Jonathan Wu <wu.jonathan09@gmail.com>
2023-10-22 04:28:32 -0400

    remove excess info from config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  locales: ['en-US'],
-  defaultLocale: 'en-US',
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['js']
-}
-env: {
-  REPLICATE_API_TOKEN: "r8_GEHXOK7RKHL0bEcqFeYu7vZVJyxXzhX36JODw"
 }
 module.exports = nextConfig
